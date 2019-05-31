ac1f5cd70d441ddee61f8fccca4176f45379276e
elenadimitrova <elenavdimitrova@gmail.com>
2019-05-31 12:37:37 +0300

    Add try/catch-es in mining client

--- packages/reputation-miner/ReputationMiner.js
+++ packages/reputation-miner/ReputationMiner.js
@@ -372,9 +372,9 @@ class ReputationMiner {
         childAdjacentReputationProof
       })
     );
-    // console.log("updateNumber", updateNumber.toString());
-    // console.log("key", key);
-    // console.log("amount", amount.toString());
+    console.log("updateNumber", updateNumber.toString());
+    console.log("key", key);
+    console.log("amount", amount.toString());
     await this.insert(key, amount, updateNumber);
   }
 
